efb359b62ed641935f6c2935f7fa913e84bb5d50
dcy <2284858868@qq.com>
2023-11-28 21:01:23 +0800

    Vue项目及Element组件学习

---
+++ vue-project1/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- <element-view></element-view> -->
+    <!-- <h1>{{message}}</h1> -->
+    
+    <!-- 引入新的组件-员工管理 -->
+    <!-- <emp-view></emp-view> -->
+    <router-view></router-view>
+
+  </div>
+</template>
+<script>
+// import EmpView from './views/tlias/EmpView.vue'
+// import ElementView from './views/element/ElementView.vue'
+export default {
+  components: { /*EmpView*/ },
+  // components: { ElementView },
+  data(){
+    return {
+      message:"Hello Vue"
+    }
+  }
+
+}
+</script>
+<style>
+
+</style>
