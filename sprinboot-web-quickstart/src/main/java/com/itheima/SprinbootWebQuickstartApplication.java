723c8151de0d9166435d4055e14e2bf4a75e12bb
dcy <2284858868@qq.com>
2023-12-04 10:31:25 +0800

    SpringBootWeb快速入门

---
+++ sprinboot-web-quickstart/src/main/java/com/itheima/SprinbootWebQuickstartApplication.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//启动类 启动springboot工程
+@SpringBootApplication
+public class SprinbootWebQuickstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinbootWebQuickstartApplication.class, args);
+    }
+
+}
